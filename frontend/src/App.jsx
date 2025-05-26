cc327047cdd4d3263b98c4c539a9b40df677dacc
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2025-05-26 19:33:35 -0400

    made the front end look a little better to help with user view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,62 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [accessToken, setAccessToken] = useState(null);
+  const [refreshToken, setRefreshToken] = useState(null);
+  const [expiresIn, setExpiresIn] = useState(null);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    const auth = urlParams.get("access_token");
+    const refresh = urlParams.get("refresh_token");
+    const expires = urlParams.get("expires_in");
+
+    if (auth) {
+      setAccessToken(auth);
+      setRefreshToken(refresh);
+      setExpiresIn(expires);
+    }
+  }, []);
+
+  function handleSignOut() {
+    setAccessToken(null);
+    setRefreshToken(null);
+    setExpiresIn(null);
+  }
+
   return (
     <>
-      <p>Hello World</p>
-      <a href="https://test-spotify-site.local:3000/login">
-        Login with Spotify
-      </a>
+      <p>Spotify Login Demo</p>
+      {accessToken ? (
+        <div>
+          <p>You are signed in</p>
+          <ul>
+            <li>Authentication Token: {`${accessToken}`}</li>
+            <li>Refresh Token: {`${refreshToken}`}</li>
+            <li>Seconds to Expiration: {`${expiresIn}`}</li>
+          </ul>
+          <div
+            onClick={handleSignOut}
+            style={{
+              color: "white",
+              background: "black",
+              width: "80px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: "12px",
+              padding: "4px 6px",
+            }}
+          >
+            Sign Out
+          </div>
+        </div>
+      ) : (
+        <a href="https://test-spotify-site.local:3000/login">
+          Login with Spotify
+        </a>
+      )}
     </>
   );
 }
